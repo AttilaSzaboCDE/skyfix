ee3def0d6f3a2c6823bbd3f96fadb0055987b5a1
Attila Szabó <143017713+AttilaSzaboCDE@users.noreply.github.com>
2025-03-21 18:40:01 +0100

    Added all html files, what needs in the project.

--- app.py
+++ app.py
@@ -2,9 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('base_index.html')
+#@app.route('/')
+#def hello_world():
+#    return render_template('base_index.html')
+
+@app.route('/monitoring')
+def monitoring():
+    return render_template('monitoring.html')
+
+@app.route('/vm_monitoring')
+def vm_monitoring():
+    return render_template('vm_monitoring.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/vnet_monitoring')
+def vnet_monitoring():
+    return render_template('vnet_monitoring.html')
+
+@app.route('/stor_monitoring')
+def stor_monitoring():
+    return render_template('stor_monitoring.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/scripts')
+def scripts():
+    return render_template('scripts.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
